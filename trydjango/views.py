dcf9c2dadc62c78e9c9ec6667ed16f6b324f137a
10nayan <nayanjyotipatowary@gmail.com>
2024-05-04 23:57:42 +0530

    Setting and views updated with db call

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
+from articles.models import Article
+import random
 
-HTMLSTRING = """
-<h1>Hello World</h1>
-"""
+RANDOM_ID = random.randint(1, 3)
 
 def home_view(request):
-    return HttpResponse(HTMLSTRING)
+    random_obj = Article.objects.get(id=RANDOM_ID)
+    html_string = f"""
+    <h1>Hello World</h1>
+    <p>Title: {random_obj.title}</p>
+    <p>Content: {random_obj.content}</p>
+    """
+    return HttpResponse(html_string)
