76b75689b71215a7c16f5d536cc9bc26565f5d64
10nayan <nayanjyotipatowary@gmail.com>
2024-05-06 00:25:33 +0530

    Form method added with template POST request

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import article_detail_view
+from .views import article_detail_view, article_search_view, article_create_view
 
 urlpatterns = [
-    path('<int:id>', article_detail_view)
+    path('<int:id>', article_detail_view),
+    path('', article_search_view),
+    path('create', article_create_view),
 ]
--- articles/views.py
+++ articles/views.py
@@ -9,3 +9,23 @@ def article_detail_view(request, id=None):
     context= {'object': article_obj}
     return render(request, 'articles/article-detail.html', context=context)
 
+def article_search_view(request):
+    article_obj = None
+    query_dict = request.GET
+    query = query_dict.get('q')
+    if query:
+        article_obj = Article.objects.get(id=query)
+    context= {'object': article_obj}
+    return render(request, 'articles/article-search.html', context=context)
+
+def article_create_view(request):
+    context = {}
+    if request.method == "POST":
+        query_dict = request.POST
+        title = query_dict.get('title')
+        content = query_dict.get('content')
+        article_object = Article.objects.create(title=title, content=content)
+        context['object'] = article_object
+        context['created'] = True
+    return render(request, 'articles/article-create.html', context=context)
+
